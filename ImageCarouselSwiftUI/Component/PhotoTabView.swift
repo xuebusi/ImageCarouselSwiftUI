dd3edb9247d59bc594606bc4f4b601a984fd8b60
xuebusi <xuebusi@qq.com>
2024-07-07 17:51:13 +0800

    代码优化

--- ImageCarouselSwiftUI/Component/PhotoTabView.swift
+++ ImageCarouselSwiftUI/Component/PhotoTabView.swift
@@ -44,63 +44,99 @@ struct PhotoTabView: View {
                     .onTapGesture {
                         print("\(index)")
                     }
-                    .gesture(
-                        DragGesture()
-                            .onChanged({ value in
-                                offsetY = value.translation.height
-                            })
-                            .onEnded({ value in
-                                if abs(offsetY) > screenHeight/3 {
-                                    withAnimation(.easeInOut(duration: 0.3)) {
-                                        offsetY = offsetY > 0 ? screenHeight : -screenHeight
-                                    }
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                                        photos.remove(at: index)
-                                        opacity = 0
-                                        offsetY = 0
-                                        scale = 0
-                                    }
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                        withAnimation {
-                                            opacity = 1
-                                            scale = 1
-                                        }
-                                    }
-                                } else {
-                                    withAnimation {
-                                        offsetY = 0
-                                    }
-                                }
-                            })
-                    )
+                    .gesture(gestureHandler(index))
             }
         }
         .tabViewStyle(PageTabViewStyle())
         .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
         .overlay {
-            VStack {
-                Text("\(currentIndex)")
-                    .font(.largeTitle)
-                HStack {
-                    Button("Prev") {
-                        if currentIndex > 0 {
-                            currentIndex -= 1
-                        }
+            if !photos.isEmpty {
+                ControlButtonView()
+            } else {
+                VStack {
+                    Text("没有图片了")
+                        .font(.headline)
+                    
+                    Button("还原") {
+                        self.photos = (1...8).map({"m\($0)"})
                     }
                     .buttonStyle(.borderedProminent)
-                    Button("Next") {
-                        if currentIndex < photos.count - 1 {
-                            currentIndex += 1
-                        }
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
-                
-                Button("还原") {
-                    self.photos = (1...8).map({"m\($0)"})
                 }
             }
         }
+    }
+    
+    func ControlButtonView() -> some View {
+        HStack {
+            Button {
+                currentIndex = max(currentIndex - 1, 0)
+            } label: {
+                Image(systemName: "chevron.left.circle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+            }
+            .tint(.white.opacity(0.6))
+            .buttonStyle(.borderless)
+            .disabled(currentIndex == 0)
+            
+            Spacer()
+            
+            Text("\(currentIndex)")
+                .font(.largeTitle)
+                .padding()
+                .background(.blue.opacity(0.5))
+                .clipShape(Circle())
+            
+            Spacer()
+            
+            Button {
+                currentIndex = min(currentIndex + 1, photos.count - 1)
+            } label: {
+                Image(systemName: "chevron.right.circle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+            }
+            .tint(.white.opacity(0.6))
+            .buttonStyle(.borderless)
+            .disabled(currentIndex == photos.count - 1)
+        }
+        .padding(.horizontal, 30)
+    }
+    
+    /// - 手势处理
+    private func gestureHandler(_ index: Int) -> some Gesture {
+        DragGesture()
+            .onChanged({ value in
+                offsetY = value.translation.height
+            })
+            .onEnded({ value in
+                if abs(offsetY) > screenHeight/3 {
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        offsetY = offsetY > 0 ? screenHeight : -screenHeight
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        photos.remove(at: index)
+                        if photos.count == 1 {
+                            currentIndex = 0
+                        }
+                        opacity = 0
+                        offsetY = 0
+                        scale = 0
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        withAnimation {
+                            opacity = 1
+                            scale = 1
+                        }
+                    }
+                } else {
+                    withAnimation {
+                        offsetY = 0
+                    }
+                }
+            })
     }
 }
 
